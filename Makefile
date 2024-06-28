3dbf7519e09a8666b93784af93750d06e2770187
Girardi <girardimatheus27@gmail.com>
2024-06-28 16:13:04 -0300

    Ajustes workoutapi

--- Makefile
+++ Makefile
@@ -2,3 +2,9 @@
 
 run:
 	@uvicorn workoutapi.main:app --reload
+
+create-migrations:
+	@PYTHONPATH=$PYTHONPATH:$(PWD) alembic revision --autogenerate -m $(d)
+
+run-migrations:
+	@PYTHONPATH=$PYTHONPATH:$(PWD) alembic upg
--- workoutapi/atleta/models.py
+++ workoutapi/atleta/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Float, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workoutapi.categorias.models import CategoriaModel
+from workoutapi.centro_treinamento.models import CentroTreinamentoModel
 from workoutapi.contrib.models import BaseModel
 
 
@@ -9,9 +11,14 @@ class AtletaModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
-    cpf: Mapped[str] = mapped_column(String(11), nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), unique=True, nullable=False)
     idade: Mapped[int] = mapped_column(Integer(11), nullable=False)
     peso: Mapped[float] = mapped_column(Float, nullable=False)
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
+    categoria_id: Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
+    centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates='atleta')
+    centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey('centro_treinamento.pk_id'))
+    
